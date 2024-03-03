66837a4d36b1874927cb3986dc0eac4d91eee809
João Azevedo <joaopsazevedo@outlook.com>
2024-03-03 22:17:33 +0000

    Add error handling to memory module

--- src/memory.rs
+++ src/memory.rs
@@ -1,6 +1,17 @@
-pub fn get_process_ids(process_name: &str) -> Vec<u32> {
-    // TODO: handle process_name with and without .exe suffix
+use crate::memory;
+use anyhow::anyhow;
 
+#[derive(thiserror::Error, Debug)]
+pub enum MemoryError {
+    #[error("failed to get processes snapshot")]
+    ProcessSnapshotFailed,
+    #[error("CS2 process not found")]
+    CS2ProcessNotFound,
+    #[error("multiple CS2 processes found (only one allowed)")]
+    MultipleCS2Processes,
+}
+
+pub fn get_process_ids(process_name: &str) -> Result<Vec<u32>, MemoryError> {
     let handle: windows_sys::Win32::Foundation::HANDLE = unsafe {
         windows_sys::Win32::System::Diagnostics::ToolHelp::CreateToolhelp32Snapshot(
             windows_sys::Win32::System::Diagnostics::ToolHelp::TH32CS_SNAPPROCESS,
@@ -9,13 +20,14 @@ pub fn get_process_ids(process_name: &str) -> Vec<u32> {
     };
 
     if handle == windows_sys::Win32::Foundation::INVALID_HANDLE_VALUE {
-        // TODO: Return error
+        return Err(MemoryError::ProcessSnapshotFailed);
     }
 
     let mut process_ids = vec![];
 
     let mut current_process = windows_sys::Win32::System::Diagnostics::ToolHelp::PROCESSENTRY32 {
-        dwSize: std::mem::size_of::<windows_sys::Win32::System::Diagnostics::ToolHelp::PROCESSENTRY32>() as u32,
+        dwSize: std::mem::size_of::<windows_sys::Win32::System::Diagnostics::ToolHelp::PROCESSENTRY32>(
+        ) as u32,
         cntUsage: 0,
         th32ProcessID: 0,
         th32DefaultHeapID: 0,
@@ -32,18 +44,49 @@ pub fn get_process_ids(process_name: &str) -> Vec<u32> {
             &mut current_process,
         ) != 0
     } {
-        let current_process_name = std::str::from_utf8(&current_process.szExeFile).unwrap();
-        //  TODO: Log if we could not convert the process .exe name to UTF16
-        if current_process_name == process_name {
-            process_ids.push(current_process.th32ProcessID)
+        match std::ffi::CStr::from_bytes_until_nul(&current_process.szExeFile) {
+            Ok(current_process_name_cstr) => match current_process_name_cstr.to_str() {
+                Ok(current_process_name) => {
+                    log::trace!("Found process {current_process_name}");
+                    if current_process_name == process_name {
+                        process_ids.push(current_process.th32ProcessID)
+                    }
+                }
+                Err(_) => {
+                    log::warn!(
+                        "failed to convert process {} name from CStr to &str",
+                        current_process.th32ProcessID
+                    )
+                }
+            },
+            Err(_) => {
+                log::warn!(
+                    "failed to get process {} name",
+                    current_process.th32ProcessID
+                )
+            }
         }
     }
 
     unsafe {
         if windows_sys::Win32::Foundation::CloseHandle(handle) == 0 {
-            // TODO: Log that we couldn't close the snapshot handle.
+            log::warn!("failed to close snapshot handle")
         }
     }
 
-    process_ids
+    Ok(process_ids)
+}
+
+pub fn get_cs2_process_id() -> Result<u32, MemoryError> {
+    let mut process_ids = memory::get_process_ids("cs2.exe")?.into_iter();
+
+    if let Some(process_id) = process_ids.next() {
+        if process_ids.next().is_some() {
+            return Err(MemoryError::MultipleCS2Processes);
+        } else {
+            Ok(process_id)
+        }
+    } else {
+        Err(MemoryError::CS2ProcessNotFound)
+    }
 }
